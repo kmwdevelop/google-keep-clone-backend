d31928e808c78400cadbfd756a403c662649fe30
곽민우 <g.kmwdev@gmail.com>
2024-09-30 11:03:16 +0900

    Feat: "express 추가 및 초기 설정"

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+
+const app = express();
+
+app.use(cors());
+app.use(methodOverride());
+app.use(express.json());
+app.use(morgan("dev")); // 로그를 찍는 모듈
+
+app.use((req, res, next) => {
+  const error = new Error("존재하지 않는 API 경로입니다.");
+  error.status = 404;
+  next(eeror);
+});
+
+app.use();
