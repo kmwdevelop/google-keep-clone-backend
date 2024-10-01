814548e7ff0bd5777f4ad8b37d3cad1434c500c5
진정한 <g.kmwdev@gmail.com>
2024-10-01 20:21:43 +0900

    Feat: "에러 핸들링 및 서버열기 코드"

--- src/app.js
+++ src/app.js
@@ -13,7 +13,19 @@ app.use(morgan("dev")); // 로그를 찍는 모듈
 app.use((req, res, next) => {
   const error = new Error("존재하지 않는 API 경로입니다.");
   error.status = 404;
-  next(eeror);
+  next(error);
 });
 
-app.use();
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status).json({ msg: error.message });
+    return;
+  }
+
+  res.status(500).json({ msg: "서버 내부 오류" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
